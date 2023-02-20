4b0b524177f8c69af7944fb2eef514d8e4546c2d
davidberget@gmail.com <davidberget@gmail.com>
2023-02-20 11:02:58 -0600

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SharkAttack.MixProject do
   def project do
     [
       app: :shark_attack,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
