24b64d44d863e88ae6ba823af5bd7661006f0f29
davidberget@gmail.com <davidberget@gmail.com>
2023-04-06 15:55:47 -0500

    fix paylink

--- lib/shark_attack/users.ex
+++ lib/shark_attack/users.ex
@@ -4,16 +4,16 @@ defmodule SharkAttack.Users do
   alias SharkAttack.Accounts.User
 
   @paylinks [
-    "63c325b709ab5e7d1dd80693",
-    "63c3264909ab5e7d1dd80906",
-    "63c314e465f452f94a19a635",
-    "63c2f71465f452f94a196437",
-    "63c8793b1e7e23ce432243d2",
-    "63c87a93f07af110f94a43c3",
-    "63c87b971e7e23ce4322481d",
-    "63c936661e7e23ce43237023",
-    "63caba0dbc4646a3c14090f4",
-    "63d2bbeb52ad37f49859a3b2",
+    # "63c325b709ab5e7d1dd80693",
+    # "63c3264909ab5e7d1dd80906",
+    # "63c314e465f452f94a19a635",
+    # "63c2f71465f452f94a196437",
+    # "63c8793b1e7e23ce432243d2",
+    # "63c87a93f07af110f94a43c3",
+    # "63c87b971e7e23ce4322481d",
+    # "63c936661e7e23ce43237023",
+    # "63caba0dbc4646a3c14090f4",
+    # "63d2bbeb52ad37f49859a3b2",
     "642f1d287a50dc6571bba6b4"
   ]
 
@@ -122,10 +122,10 @@ defmodule SharkAttack.Users do
 
   def get_purchases() do
     SharkAttack.Helpers.do_get_request(
-      "https://api.hel.io/v1/export/payments?publicKey=BS61tv1KbsPhns3ppU8pmWozfReZjhxFL2MPhBdDWNEm",
+      "https://api.hel.io/v1/export/payments?publicKey=Labsrz67AZ3Z7zk76k8hWh38nZSsfgUn4VtSU6ZdWL3",
       [
         {"authorization",
-         "Bearer ro26WikjG6auKhJTh+csZ83XRwI9C28fk2tOdVfrOUYdhV79zvDSAAOnG8YiRr8CLfbPfg1HbYG6QOJyG2QeqJlCdH3FZ/+qnV+Agnz1gb1FyumfmKuC4w3eHnjkVnVZ"}
+         "Bearer n24Y98PGn/38OZx0vU81pCSVBV6ZvVjYlbkBi1owTOSq+Yt8r75k1jkKVQzN4z6icCOi5M/yDYLbwIN90wC9YN6CRWTxo9yS+bzVBSJX860GRIBK7DJWC7upbDSj44Do"}
       ]
     )
     |> Enum.filter(fn %{"paymentRequestId" => paymentId} -> paymentId in @paylinks end)
